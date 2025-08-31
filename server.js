e3522cc1ce336379bc4494b5c88943c5755df8bd
Jafar Touzeau <jafar.touzeau@gmail.com>
2025-08-31 11:45:42 +0200

    Add redis to have a stateless server.

--- server.js
+++ server.js
@@ -2,28 +2,45 @@ const express = require('express');
 const { join } = require('node:path');
 const { createServer } = require('node:http');
 const { Server } = require('socket.io');
+const Redis = require('ioredis');
 
 const PORT = 3000;
-let GLOBAL_COUNTER = 0;
+const GLOBAL_COUNTER = 'global_counter';
+
 const app = express();
 const server = createServer(app);
 const io = new Server(server);
+const redis = new Redis(process.env.REDIS_URL);
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
-io.on('connection', (socket) => {
+
+io.on('connection', async (socket) => {
   console.log('a user connected');
-  io.emit('update counter', GLOBAL_COUNTER);
-  socket.on('incr', () => {
-    GLOBAL_COUNTER++;
-    io.emit('update counter', GLOBAL_COUNTER);
+
+  // Get counter if first connection
+  const currentValue = await redis.get(GLOBAL_COUNTER) || 0;
+  io.emit('update counter', parseInt(currentValue));
+
+  // +1
+  socket.on('incr', async () => {
+    // Incr is an axtomic operation with Redis
+    const newValue = await redis.incr(GLOBAL_COUNTER);
+    io.emit('update counter', newValue);
   });
-  socket.on('decr', () => {
-    GLOBAL_COUNTER = GLOBAL_COUNTER>0 ? GLOBAL_COUNTER-1 : 0;
-      io.emit('update counter', GLOBAL_COUNTER);
-  });
+
+  // -1
+  socket.on('decr', async () => {
+    let newValue = await redis.decr(GLOBAL_COUNTER);
+    if (newValue < 0) {
+      await redis.set(GLOBAL_COUNTER, 0);
+      newValue = 0;
+    }
+    io.emit('update counter', newValue);
+  })
 });
+
 server.listen(PORT, () => {
-  console.log("server running at http://localhost:3000");
+  console.log(`Server is running on port ${PORT}`);
 });
